2b0059dff6225a7bddc57917068fca14ea1f0186
Joshua Gigg <giggsey@gmail.com>
2026-05-19 17:13:58 +0100

    :arrow_up: Symfony 8 support

--- src/PSR7StreamResponse.php
+++ src/PSR7StreamResponse.php
@@ -1,206 +1,106 @@
 <?php
-/**
- *
- * User: giggsey
- * Date: 08/08/18
- * Time: 13:21
- */
 
 namespace giggsey\PSR7StreamResponse;
 
+use LogicException;
 use Psr\Http\Message\StreamInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PSR7StreamResponse extends Response
 {
-    /**
-     * @var StreamInterface
-     */
-    protected $stream;
-    protected $mimeType;
-    protected $offset;
-    protected $maxlen;
+    protected StreamInterface $stream;
+    protected int $offset = 0;
+    protected int $maxlen = -1;
 
-    public function __construct(
-        StreamInterface $stream,
-        $mimeType,
-        $status = 200,
-        $headers = array(),
-        $public = true
-    ) {
+    public function __construct(StreamInterface $stream, int $status = 200, array $headers = [])
+    {
         parent::__construct(null, $status, $headers);
-
-        $this->setStream($stream, $mimeType);
-
-        if ($public) {
-            $this->setPublic();
-        }
-    }
-
-    /**
-     * Sets the file to stream.
-     *
-     * @param StreamInterface $stream
-     * @param string $mimeType
-     *
-     * @return $this
-     */
-    public function setStream(StreamInterface $stream, $mimeType)
-    {
         $this->stream = $stream;
-        $this->mimeType = $mimeType;
 
-        return $this;
-    }
+        // Automatically set Content-Length if the stream knows its size
+        $size = $stream->getSize();
+        if ($size !== null && !$this->headers->has('Content-Length')) {
+            $this->headers->set('Content-Length', (string) $size);
+        }
 
-    /**
-     * @return StreamInterface
-     */
-    public function getStream()
-    {
-        return $this->stream;
-    }
-
-    /**
-     * Sets the Content-Disposition header with the given filename.
-     *
-     * @param string $disposition ResponseHeaderBag::DISPOSITION_INLINE or ResponseHeaderBag::DISPOSITION_ATTACHMENT
-     * @param string $filename Use this UTF-8 encoded filename instead of the real name of the file
-     *
-     * @return $this
-     */
-    public function setContentDisposition($disposition, $filename)
-    {
-        $dispositionHeader = $this->headers->makeDisposition($disposition, $filename);
-        $this->headers->set('Content-Disposition', $dispositionHeader);
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function prepare(Request $request): static
-    {
-        $this->headers->set('Content-Length', $this->stream->getSize());
-
+        // Indicate to the client that we accept Range requests
         if (!$this->headers->has('Accept-Ranges')) {
-            // Only accept ranges on safe HTTP methods
-            $this->headers->set('Accept-Ranges', $request->isMethodSafe(false) ? 'bytes' : 'none');
+            $this->headers->set('Accept-Ranges', 'bytes');
         }
-
-        if (!$this->headers->has('Content-Type')) {
-            $this->headers->set('Content-Type', $this->mimeType ?: 'application/octet-stream');
-        }
-
-        if ('HTTP/1.0' !== $request->server->get('SERVER_PROTOCOL')) {
-            $this->setProtocolVersion('1.1');
-        }
-
-        $this->ensureIEOverSSLCompatibility($request);
-
-        $this->offset = 0;
-        $this->maxlen = -1;
-
-        if ($request->headers->has('Range')) {
-            // Process the range headers.
-            if (!$request->headers->has('If-Range') || $this->hasValidIfRangeHeader($request->headers->get('If-Range'))) {
-                $range = $request->headers->get('Range');
-                $fileSize = $this->stream->getSize();
-
-                list($start, $end) = explode('-', substr($range, 6), 2) + array(0);
-
-                $end = ('' === $end) ? $fileSize - 1 : (int)$end;
-
-                if ('' === $start) {
-                    $start = $fileSize - $end;
-                    $end = $fileSize - 1;
-                } else {
-                    $start = (int)$start;
-                }
-
-                if ($start <= $end) {
-                    if ($start < 0 || $end > $fileSize - 1) {
-                        $this->setStatusCode(416);
-                        $this->headers->set('Content-Range', sprintf('bytes */%s', $fileSize));
-                    } elseif (0 !== $start || $end !== $fileSize - 1) {
-                        $this->maxlen = $end < $fileSize ? $end - $start + 1 : -1;
-                        $this->offset = $start;
-
-                        $this->setStatusCode(206);
-                        $this->headers->set('Content-Range', sprintf('bytes %s-%s/%s', $start, $end, $fileSize));
-                        $this->headers->set('Content-Length', $end - $start + 1);
-                    }
-                }
-            }
-        }
-
-        return $this;
-    }
-
-
-    private function hasValidIfRangeHeader($header)
-    {
-        if ($this->getEtag() === $header) {
-            return true;
-        }
-
-        if (null === $lastModified = $this->getLastModified()) {
-            return false;
-        }
-
-        return $lastModified->format('D, d M Y H:i:s') . ' GMT' === $header;
     }
 
     /**
-     * Sends the file.
-     *
-     * {@inheritdoc}
+     * Symfony 8 requires a static return type.
      */
-    public function sendContent(): static
-    {
-        if (!$this->isSuccessful()) {
-            return parent::sendContent();
-        }
-
-        if (0 === $this->maxlen) {
-            return $this;
-        }
-
-        $this->stream->seek($this->offset);
-
-        if ($this->maxlen === -1) {
-            // Read the entire stream
-            $this->maxlen = $this->stream->getSize() - $this->offset;
-        }
-
-        echo $this->stream->read($this->maxlen);
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @throws \LogicException when the content is not null
-     */
-    public function setContent($content): static
+    public function setContent(mixed $content): static
     {
         if (null !== $content) {
-            throw new \LogicException('The content cannot be set on a PSR7StreamResponse instance.');
+            throw new LogicException('Content cannot be set directly on a PSR7StreamResponse instance.');
         }
 
         return $this;
     }
 
     /**
-     * {@inheritdoc}
-     *
-     * @return false
+     * Symfony 8 requires string|false return type.
      */
     public function getContent(): string|false
     {
         return false;
+    }
+
+    /**
+     * Symfony 8 requires a static return type.
+     */
+    public function sendContent(): static
+    {
+        if (!$this->isSuccessful() && !$this->isRedirection()) {
+            return parent::sendContent();
+        }
+
+        if ($this->stream->isSeekable()) {
+            $this->stream->seek($this->offset);
+        }
+
+        $length = $this->maxlen;
+
+        while (!$this->stream->eof()) {
+            $readSize = $length !== -1 ? min(8192, $length) : 8192;
+            $chunk = $this->stream->read($readSize);
+
+            if ($chunk === '') {
+                break;
+            }
+
+            echo $chunk;
+            flush();
+
+            if ($length !== -1) {
+                $length -= strlen($chunk);
+                if ($length <= 0) {
+                    break;
+                }
+            }
+        }
+
+        $this->stream->close();
+
+        return $this;
+    }
+
+    /**
+     * Example fluent method for Range Handling (custom to your library).
+     * Make sure all your fluent setters return `static` to remain compliant.
+     */
+    public function setOffset(int $offset): static
+    {
+        $this->offset = $offset;
+        return $this;
+    }
+
+    public function setMaxlen(int $maxlen): static
+    {
+        $this->maxlen = $maxlen;
+        return $this;
     }
 }
---
+++ tests/PSR7StreamResponseTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace giggsey\PSR7StreamResponse\Tests;
+
+use Giggsey\PSR7StreamResponse\PSR7StreamResponse;
+use GuzzleHttp\Psr7\Utils;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class PSR7StreamResponseTest extends TestCase
+{
+    public function testStreamsEntireContentCorrectly(): void
+    {
+        $string = 'Hello, Symfony 8!';
+        $stream = Utils::streamFor($string);
+
+        $response = new PSR7StreamResponse($stream);
+
+        ob_start();
+        $response->sendContent();
+        $output = ob_get_clean();
+
+        $this->assertSame($string, $output);
+    }
+
+    public function testStreamsWithOffsetAndMaxlen(): void
+    {
+        $string = '0123456789';
+        $stream = Utils::streamFor($string);
+
+        $response = new PSR7StreamResponse($stream);
+        // Assuming you use these methods to handle ranges
+        $response->setOffset(2)->setMaxlen(5);
+
+        ob_start();
+        $response->sendContent();
+        $output = ob_get_clean();
+
+        // Should start at index 2 and read 5 bytes
+        $this->assertSame('23456', $output);
+    }
+
+    public function testSetsContentLengthHeaderAutomatically(): void
+    {
+        $string = '12345';
+        $stream = Utils::streamFor($string);
+
+        $response = new PSR7StreamResponse($stream);
+
+        $this->assertTrue($response->headers->has('Content-Length'));
+        $this->assertSame('5', $response->headers->get('Content-Length'));
+    }
+
+    public function testDoesNotOverwriteExistingContentLengthHeader(): void
+    {
+        $string = '12345';
+        $stream = Utils::streamFor($string);
+
+        $response = new PSR7StreamResponse($stream, 200, ['Content-Length' => '99']);
+
+        $this->assertSame('99', $response->headers->get('Content-Length'));
+    }
+
+    public function testThrowsExceptionWhenSettingContent(): void
+    {
+        $stream = Utils::streamFor('test');
+        $response = new PSR7StreamResponse($stream);
+
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Content cannot be set directly on a PSR7StreamResponse instance.');
+
+        $response->setContent('trying to break it');
+    }
+
+    public function testGetContentReturnsFalse(): void
+    {
+        $stream = Utils::streamFor('test');
+        $response = new PSR7StreamResponse($stream);
+
+        $this->assertFalse($response->getContent());
+    }
+}
